9b08b405dbed0cb2df65a39022024ec7ec92450c
hossamk <hossams.k@gmail.com>
2016-06-19 17:05:34 +0200

    Implemented onCreate. read email and password and call login method

--- app/src/main/java/com/smartuniversitygp/smartuniversity/LoginActivity.java
+++ app/src/main/java/com/smartuniversitygp/smartuniversity/LoginActivity.java
@@ -1,13 +1,56 @@
 package com.smartuniversitygp.smartuniversity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText etEmail;
+    private EditText etPassword;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        // Login button Click Event
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                // Check for empty data in the form
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    // login user
+                    login(email, password);
+                } else {
+                    // Prompt user to enter credentials
+                    Toast.makeText(getApplicationContext(), "Please enter your email and password!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
     }
+
+    private void login(final String email, final String password){
+
+    }
+
 }
