82eecc751a825daa9eb14fdac145d90385727de3
hossamk <hossams.k@gmail.com>
2016-06-19 18:45:44 +0200

    Updated SessionManager to use User class

--- app/src/main/java/com/smartuniversitygp/smartuniversity/helper/SessionManager.java
+++ app/src/main/java/com/smartuniversitygp/smartuniversity/helper/SessionManager.java
@@ -25,6 +25,12 @@ public class SessionManager {
 
     private static final String KEY_IS_LOGGEDIN = "isLoggedIn";
     private static final String KEY_TOKEN = "loginToken";
+    private static final String KEY_FIRST = "loginFirstName";
+    private static final String KEY_LAST = "loginLastName";
+    private static final String KEY_EMAIL = "loginEmail";
+    private static final String KEY_TYPE = "loginType";
+
+
 
     public SessionManager(Context context) {
         this._context = context;
@@ -32,9 +38,13 @@ public class SessionManager {
         editor = pref.edit();
     }
 
-    public void setLogin(boolean isLoggedIn, String token) {
+    public void setLogin(boolean isLoggedIn, User user) {
         editor.putBoolean(KEY_IS_LOGGEDIN, isLoggedIn);
-        editor.putString(KEY_TOKEN, token);
+        editor.putString(KEY_TOKEN, user.getToken());
+        editor.putString(KEY_FIRST, user.getFirstName());
+        editor.putString(KEY_LAST, user.getLastName());
+        editor.putString(KEY_EMAIL, user.getEmail());
+        editor.putString(KEY_TYPE, user.getType());
 
         // commit changes
         editor.commit();
@@ -50,6 +60,17 @@ public class SessionManager {
 
     public boolean isLoggedIn(){
         return pref.getBoolean(KEY_IS_LOGGEDIN, false);
+    }
+
+    public User getUser(){
+        String first = pref.getString(KEY_FIRST, "");
+        String last = pref.getString(KEY_LAST, "");
+        String email = pref.getString(KEY_EMAIL, "");
+        String type = pref.getString(KEY_TYPE, "");
+        String token = pref.getString(KEY_TOKEN, "");
+
+        User user = new User(first, last, email, type, token);
+        return user;
     }
 
     public String getToken(){
