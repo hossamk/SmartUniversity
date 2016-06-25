673fe1a85639642f88e0d195281a7fb7063e4d5b
hossamk <hossams.k@gmail.com>
2016-06-25 03:45:24 +0200

    Added project number

--- app/src/main/java/com/smartuniversitygp/smartuniversity/app/AppConfig.java
+++ app/src/main/java/com/smartuniversitygp/smartuniversity/app/AppConfig.java
@@ -4,6 +4,11 @@ package com.smartuniversitygp.smartuniversity.app;
  * Created by hossam on 6/19/16 .
  */
 public class AppConfig {
+
+    //GCM PROJECT NBUMBER
+    final static String PROJECT_NUMBER="855262990562";
     // login url
     public static String URL_LOGIN = "http://www.smartuniversitygp.com/request/login.php";
+
+
 }
