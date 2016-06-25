c960edd0b2716650c4674527cb48e3813b941691
hossamk <hossams.k@gmail.com>
2016-06-25 07:27:22 +0200

    new configs for notifications

--- app/src/main/java/com/smartuniversitygp/smartuniversity/app/AppConfig.java
+++ app/src/main/java/com/smartuniversitygp/smartuniversity/app/AppConfig.java
@@ -7,6 +7,10 @@ public class AppConfig {
 
     //GCM PROJECT NBUMBER
     public final static String PROJECT_NUMBER="855262990562";
+    //id to handle notifications
+    public final static int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
     // login url
     public static String URL_LOGIN = "http://www.smartuniversitygp.com/request/login.php";
     // GCM REGISTRATION
